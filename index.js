a75ca3a452ba1d2cd125ef6ce3a22bcfa26b0cdd
Replit user <>
2023-05-23 14:31:28 +0000

    Implement timestamp functionality

--- index.js
+++ index.js
@@ -2,13 +2,14 @@
 // where your node app starts
 
 // init project
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+const cors = require('cors');
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
+;  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -20,11 +21,43 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get('/api/:timestamp?', (req, res) => {
+  try {
+    let date;
+
+    if (req.params.timestamp === undefined) {
+      date = new Date();
+    } else {
+      const { timestamp } = req.params;
+      const parsedTimestamp = /^\d+$/.test(timestamp) ? parseInt(timestamp) : timestamp;
+      date = new Date(parsedTimestamp);
+    }
+
+    if (isNaN(date.getTime())) {
+      return res.json({ error: 'Invalid Date' });
+    }
+
+    res.json({
+      unix: date.getTime(),
+      utc: date.toUTCString()
+    });
+  } catch (error) {
+    return res.status(500).json({ error: 'An error occurred' });
+  }
 });
 
+// Handle empty date parameter
+app.get('/api/', (req, res) => {
+  const date = new Date();
 
+// Prepare the response object
+  const response = {
+    unix: date.getTime(),
+    utc: date.toUTCString()
+  };
+
+  res.json(response);
+});
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
